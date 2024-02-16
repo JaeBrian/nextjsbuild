6e4a6b67bc2a71913379c6668ce50dbbc5190dfb
Brian Lee <bnlee419@gmail.com>
2024-02-16 04:55:23 -0800

    nextjs cssmodules, clsx, Image/from nextjs

---
+++ nextjs-dashboard/app/layout.tsx
@@ -0,0 +1,15 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
+//added inter.className import from fonts.ts that applies the smoothed out font globally
