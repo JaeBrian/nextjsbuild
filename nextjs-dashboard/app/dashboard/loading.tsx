996a0e52d5e4faab8f0d69c98b09e105dc2e90a6
Brian Lee <bnlee419@gmail.com>
2024-02-17 16:54:35 -0800

    added dashboard skeleton

--- nextjs-dashboard/app/dashboard/loading.tsx
+++ nextjs-dashboard/app/dashboard/loading.tsx
@@ -1,5 +1,7 @@
+import DashboardSkeleton from '../ui/skeletons';
+
 export default function Loading() {
-  return <div>Loading...</div>;
+  return <DashboardSkeleton />;
 }
 
-//uses suspesne which means, it waits for the data to be available until it renders.
+//uses suspense which means, it waits for the data to be available until it renders.
